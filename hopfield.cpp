fa89f758986ec9c618f69a19c225cf909a99d0f4
Dragonpilot07 <mvsarjun7@gmail.com>
2025-02-19 14:47:42 +0530

    Add files via upload

---
+++ hopfield.cpp
@@ -0,0 +1,76 @@
+#include "hopfield_class.h"
+#include <iostream>
+#include <random>
+#include <vector>
+
+using namespace std;
+vector<vector<int>> trainHopfield(const vector<vector<int>> &memories){
+    int n=image_size;
+    vector<vector<int>> weights(n,vector<int>(n,0));
+    for(const auto &memory:memories){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j){
+                    weights[i][j]=memory[i]*memory[j];
+                }
+            }
+        }
+    }
+    return weights;
+}
+vector<int> corruptMemory(const vector<int> &memory,double p){
+    random_device rd;
+    mt19937 gen(rd());
+    bernoulli_distribution dist(p);
+    vector<int> corrupted=memory;
+    for(int i=0;i<image_size;i++){
+        if(dist(gen)){
+            corrupted[i]*=-1;
+        }
+    }
+    return corrupted;
+}
+vector<int> synchronousUpdate(const vector<int> &state,const vector<vector<int>> &weights){
+    int n=image_size;
+    vector<int> newState(n);
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=0;j<n;j++){
+            sum+=weights[i][j]*state[j];
+        }
+        newState[i]=(sum>=0)? 1:-1;
+    }
+    return newState;
+}
+vector<int> asynchronousUpdate(vector<int> state,const vector<vector<int>> &weights){
+    int n=image_size;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(0,n-1);
+    for(int k=0;k<n;k++){
+        int i=dist(gen);
+        int sum=0;
+        for(int j=0;j<n;j++){
+            sum+=weights[i][j]*state[j];
+        }
+        state[i]=(sum>=0)?1:-1;
+    }
+    return state;
+}
+vector<int> cropMemory(const vector<int> &memory,int crop_size){
+    if(crop_size>=w||crop_size<=0){
+        cerr<<"Invalid crop size. Give in the range of 0 and"<<w<<endl;
+        return memory;
+    }
+    vector<int> cropped_memory=memory;
+    int sr=(h-crop_size)/2;
+    int sc=(w-crop_size)/2;
+    for(int row=0;row<h;row++){
+        for(int col=0;col<w;col++){
+            if(row<sr||row>=sr+crop_size||col<sc||col>=sc+crop_size){
+                cropped_memory[row*w+col]=-1;
+            }
+        }
+    }
+    return cropped_memory;
+}
---
+++ hopfield_class.h
@@ -0,0 +1,15 @@
+
+#ifndef HOPFIELD_CLASS_H
+#define HOPFIELD_CLASS_H
+#include <iostream>
+#include <vector>
+using namespace std;
+const int w=16;
+const int h=16;
+const int image_size=h*w;
+vector<vector<int>> trainHopfield(const vector<vector<int>> &memories);
+vector<int> corruptMemory(const vector<int> &memory,double p);
+vector<int> synchronousUpdate(const vector<int> &state,const vector<vector<int>> &weights);
+vector<int> asynchronousUpdate(vector<int> state, const vector<vector<int>> &weights);
+vector<int> cropMemory(const vector<int> &memory,int crop_size);
+#endif
