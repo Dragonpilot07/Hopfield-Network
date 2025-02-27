5ccec520d3d40cf8743f5563d964c1a3090b7074
Dragonpilot07 <mvsarjun7@gmail.com>
2025-02-27 15:41:38 +0530

    Update hopfield_class.h

--- hopfield_class.h
+++ hopfield_class.h
@@ -9,7 +9,7 @@ const int h=16;
 const int image_size=h*w;
 vector<vector<int>> trainHopfield(const vector<vector<int>> &memories);
 vector<int> corruptMemory(const vector<int> &memory,double p);
-vector<int> synchronousUpdate(const vector<int> &state,const vector<vector<int>> &weights);
+pair<vector<int>, int> synchronousUpdate(const vector<int> &state, const vector<vector<int>> &weights);
 vector<int> asynchronousUpdate(vector<int> state, const vector<vector<int>> &weights);
 vector<int> cropMemory(const vector<int> &memory,int crop_size);
 void normalizeWeights(vector<vector<int>>& weights, int numPatterns);
